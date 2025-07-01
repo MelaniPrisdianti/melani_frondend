428eae01eee029f98830729bb8d0a41eb03fb243
MelaniPrisdianti <andicici3010@gmail.com>
2025-07-01 09:48:51 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,7 @@ function Home() {
       <h1 className="display-4 fw-bold mb-3">Halo, Melani Prisdianti</h1>
     <h1 className="display-4 fw-bold mb-3">Nim:STI202202696 </h1>
       <p className="lead">Saya kuliah di kampus STMIK WIDYA UTAMA PURWOKERTO</p>
-      <img
-        src="/foto-profil.png"
-        alt="Mas Ganteng"
-        className="img-fluid rounded-circle mt-4"
-        style={{ maxWidth: '200px' }}
-      />
+     
     </div>
   );
 }
